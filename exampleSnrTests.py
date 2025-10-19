a5fe6135eb5667b0a69a6cb2eba34c3b505f84a1
Ario Kian <97977902+ArioKian@users.noreply.github.com>
2025-10-19 19:48:43 +0330

    Add files via upload

---
+++ exampleSnrTests.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+import scipy as sc
+import scipy.stats as stats
+from SignalToNoiseRatio import SnrOnAES128 as SNR
+
+plains = pd.read_csv("plaintexts.csv", header=None).values
+powerTraces = pd.read_csv("traces.csv", header=None).values
+keys = pd.read_csv("key_forSNR.csv", header=None).values
+keys = keys[0] 
+
+snrObj = SNR()
+
+snrObj.SetPlainTexts(plains)
+snrObj.SetPowerTraces(powerTraces)
+snrObj.SetCorrectKeys(keys)
+
+returnedPlain = snrObj.GetPlainTexts()
+returnedTraces = snrObj.GetPowerTraces()
+returnedkeys = snrObj.GetCorrectKeys()
+
+print("returnedPlain.shape=", returnedPlain.shape)
+print("returnedTraces.shape=", returnedTraces.shape)
+print("returnedkeys.shape=", returnedkeys.shape)
+
+# snrObj.SNRforTargetByte(1,"pooled")
+snrObj.SNRforAllBytes("pooled")
